32ceb1778ff04730fcdb9edf8b7446334da4d920
indirakalagara <kindira@gmail.com>
2022-06-06 11:30:31 +0530

    test local
    Signed-off-by: indirakalagara <kindira@gmail.com>

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ locals {
 module "setup_clis" {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
   clis   = ["yq", "jq", "igc", "rosa"]
-  # clis   = ["helm", "rosa"]
+  
 }
 
 
